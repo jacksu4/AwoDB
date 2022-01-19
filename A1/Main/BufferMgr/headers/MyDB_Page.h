0a4f8a6f990e0c87569e73eb3ee65e23a0c2bfb6
jacksu4 <jingches@uci.edu>
2022-01-19 17:15:29 -0600

    added page skeletons

---
+++ A1/Main/BufferMgr/headers/MyDB_Page.h
@@ -0,0 +1,15 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+class MyDB_Page {
+    public:
+        //constructor
+        MyDB_Page();
+        //destructor
+        ~MyDB_Page();
+
+    private:
+        //add code
+};
+
+#endif //PAGE_H
---
+++ A1/Main/BufferMgr/source/MyDB_Page.cc
@@ -0,0 +1,15 @@
+#ifndef PAGE_C
+#define PAGE_C
+
+#include "../headers/MyDB_Page.h"
+
+MyDB_Page :: MyDB_Page() {
+
+}
+
+MyDB_Page :: ~MyDB_Page() {
+
+}
+
+#endif //PAGE_H
+
