376ffb2da1e3c268ceac80fa71c3f5899cc64085
Qiaomu Zhang <qz40@pyrite.clear.rice.edu>
2022-03-21 03:46:44 -0500

    Finish BPlusSelection (undebugged)

--- A6/Main/RelOps/source/BPlusSelection.cc
+++ A6/Main/RelOps/source/BPlusSelection.cc
@@ -4,10 +4,55 @@
 
 #include "BPlusSelection.h"
 
-BPlusSelection :: BPlusSelection (MyDB_BPlusTreeReaderWriterPtr, MyDB_TableReaderWriterPtr,
-                MyDB_AttValPtr, MyDB_AttValPtr,
-                string, vector <string>) {}
+BPlusSelection :: BPlusSelection (MyDB_BPlusTreeReaderWriterPtr inputIn, MyDB_TableReaderWriterPtr outputIn,
+                MyDB_AttValPtr low, MyDB_AttValPtr high,
+                string selectionPredicateIn, vector <string> projectionsIn) {
+                    input = inputIn;
+                    output = outputIn;
+                    low = lowIn;
+                    high = highIn;
+                    selectionPredicate = selectionPredicateIn;
+                    projections = projectionsIn;
 
-void BPlusSelection :: run () {}
+                }
+
+void BPlusSelection :: run () {
+    
+    // Initialize input and output Record objects
+    MyDB_RecordPtr rec = input.getEmptyRecord ();
+    MyDB_RecordPtr outRec = output.getEmptyRecord ();
+
+    // Compile for the predicate
+    func pred = rec->compileComputation (selectionPredicate);
+
+    // Get data range from the input table
+    MyDB_RecordIteratorAltPtr myIter = input.getSortedRangeIteratorAlt(low, high);
+
+    // Set up attributes after projection
+    vector <func> finalComputations;
+	for (string s : projections) {
+		finalComputations.push_back (rec->compileComputation (s));
+	}
+
+    // Loop to select
+    while (myIter->advance ()) {
+		myIter->getCurrent (rec);
+
+        // Check predicate result
+        if(!pred ()->toBool ()) {
+            continue;
+        }
+
+        // Do projection
+        int i = 0;
+        for (auto &f : finalComputations) {
+            outRec->getAtt (i++)->set (f());
+        }
+
+        // Write
+        outRec->recordContentHasChanged ();
+        output->append (outputRec);	
+    }
+}
 
 #endif
